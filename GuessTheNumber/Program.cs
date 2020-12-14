b30ae7dd56972ff0c9ff626c57c3bdaf8b188650
BORIS-DESKTOP\boris <Boris>
2020-12-14 18:37:12 +0200

    Project finished

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GuessTheNumber
 {
@@ -8,20 +10,30 @@ namespace GuessTheNumber
         {
             //Random three digit-number
             int Min = 0;
+            int Min2 = 1;
             int Max = 9;
             int[] randomNumber = new int[3];
+            List<int> aux = new List<int>();
 
             //User
-            int userNumber = 233;
+            int userNumber = 0;
+            bool win = false;
             int life = 5;
             bool lose = false;
 
 
             Random randNum = new Random();
             for (int i = 0; i <randomNumber.Length ; i++)
-            {
-
-                randomNumber[i] = randNum.Next(Min, Max);
+            {   
+                if (i == 0)
+                {
+                    randomNumber[i] = randNum.Next(Min2, Max);
+                }
+                else
+                {
+                    randomNumber[i] = randNum.Next(Min, Max);
+                }
+                aux.Add(randomNumber[i]);
                 Console.Write(randomNumber[i]);
             }
             Console.WriteLine();
@@ -29,7 +41,7 @@ namespace GuessTheNumber
             Console.WriteLine("Random number was generated successfully");
 
             //User Input
-            while (life >= 0 && lose == false) {
+            while (life >= 0 && lose == false && win == false) {
 
                 if (life > 0)
                 {
@@ -42,17 +54,58 @@ namespace GuessTheNumber
                             userNumber = Int32.Parse(Console.ReadLine());
 
 
-
                             if (userNumber < 100 || userNumber > 999)
                             {
                                 Console.WriteLine("Please enter only 3 digit numbers!");
                                 Console.WriteLine();
                             }
                             else
-                            {
-                                life--;
-                                Console.WriteLine("Life left: " + life);
-                                Console.WriteLine("User number is " + userNumber);
+                            {    
+                                //Console.WriteLine("User number is " + userNumber);
+                                int firstNum = userNumber / 100;
+                                int secondNum = userNumber / 10;
+                                secondNum = secondNum % 10;
+                                int thirdNum = userNumber % 10;
+                                //Console.WriteLine(firstNum);
+                                //Console.WriteLine(secondNum % 10);
+                                //Console.WriteLine(thirdNum);
+
+                                var firstItem = aux.ElementAt(0);
+                                var secondItem = aux.ElementAt(1);
+                                var thirdItem = aux.ElementAt(2);
+
+                                bool numberGuessed = aux.Any(item => item == firstNum || item == secondNum || item == thirdNum);
+                               // Console.WriteLine(firstNumFromRandom);
+
+                                if (firstNum == firstItem && secondNum == secondItem && thirdNum == thirdItem)
+                                {
+                                    win = true;
+                                    Console.WriteLine("You guessed the number!");
+                                }
+                                else if (firstNum == firstItem && secondNum == secondItem || firstNum == firstItem && thirdNum == thirdItem || secondNum == secondItem && thirdNum == thirdItem)
+                                {
+                                    life--;
+                                    Console.WriteLine("Life left: " + life);
+                                    Console.WriteLine("You guessed two numbers and there position");
+                                }
+                                else if (firstNum == firstItem || secondNum == secondItem || thirdNum == thirdItem)
+                                {
+                                    life--;
+                                    Console.WriteLine("Life left: " + life);
+                                    Console.WriteLine("You guessed one numbers and position");
+                                }
+                                else if (numberGuessed == true)
+                                {
+                                    life--;
+                                    Console.WriteLine("Life left: " + life);
+                                    Console.WriteLine("You guessed one number but not the position");
+                                }
+                                else
+                                {
+                                    life--;
+                                    Console.WriteLine("Life left: " + life);
+                                    Console.WriteLine("No matches");
+                                }
                             }
                         }
                         catch (Exception)
@@ -70,9 +123,12 @@ namespace GuessTheNumber
                     Console.WriteLine();
                     Console.WriteLine("You Lose!");
                 }
-            }
-
-            
+                else if(win == true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("You win!");
+                }
+            }        
         }
     }
 }
